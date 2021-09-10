1224f08d04956beaff92a9d7a18fee90e15ad09d
DESKTOP-SP598P2\terad <terad321@gmail.com>
2021-09-10 13:47:44 +0300

    - code refactoring - editing authorization test. had problems with "testng"

--- src/main/java/org/driver/DriverHolder.java
+++ src/main/java/org/driver/DriverHolder.java
@@ -1,7 +1,6 @@
 package org.driver;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
--- src/main/java/org/infrastructure/Url.java
+++ src/main/java/org/infrastructure/Url.java
@@ -2,7 +2,7 @@ package org.infrastructure;
 
 public class Url {
 
-    public static final String BASE_URL = "https://roundpier-3f4d8.web.app";
+    public static final String BASE_URL = "https://www.dev.roundpier.com";
     public static final String my_message = BASE_URL + "/messages";
     public static final String my_account = BASE_URL + "/_gbkb/andrey-%2B221";
 }
