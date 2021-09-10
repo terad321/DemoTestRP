8020e18ea6af3916363ebcd9215e77477537a34f
DESKTOP-SP598P2\terad <terad321@gmail.com>
2021-09-10 16:37:03 +0300

    - code refactoring - editing authorization test. had problems with "testng"

--- src/test/java/org/createPost/CreatePostTest.java
+++ src/test/java/org/createPost/CreatePostTest.java
@@ -30,7 +30,8 @@ public class CreatePostTest extends BaseTest {
 
         goToUrl(Url.my_account); //Переходим на страницу профиля для создания поста
         AddPost addPost = new AddPost().createText(uuidAsString).clickCreatePost();
-        Thread.sleep(500); // Делаем ожидание для того чтобы получить созданый пост в список постов
+
+        Thread.sleep(5000); // Делаем ожидание для того чтобы получить созданый пост в список постов
         Assert.assertEquals(addPost.getListPost(), uuidAsString); //Проверяем текст созданого поста
     }
 }
